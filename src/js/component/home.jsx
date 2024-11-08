9bea29c6b56c979c740ad44cb6560dbd851e3f52
Andidela <andidela83@gmail.com>
2024-11-08 00:54:22 +0000

    proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import Card from './card'
-import SecondsCounter from "./secondscounter";
 
 
-//include images into your bundle
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<>
-			<SecondsCounter />
-			<Card />
-		</>
+		<div className="simpleCounter d-flex">
+			<div className="row fondo">
+				<div className="col counter"><i className="far fa-clock"></i></div>
+				<div className="col counter">{props.digitFour}</div>
+				<div className="col counter">{props.digitThree}</div>
+				<div className="col counter">{props.digitTwo}</div>
+				<div className="col counter">{props.digitOne}</div>
+			</div>
+		</div>
 
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,17 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+
 
 let sec = 0;
 setInterval (() => {
-    const one = Math.floor((sec/1)%10)
+    const one = Math.floor((sec/1)%10);
+    const two = Math.floor((sec/10)%10);
+    const three = Math.floor((sec/100)%10);
+    const four = Math.floor((sec/1000)%10)
     sec +=1;
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home digitOne={one} digitTwo={two} digitThree={three} digitFour={four}/>);
 },1000)
+
+//render your react application
 
